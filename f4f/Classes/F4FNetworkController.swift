d562d7226cd5af90616a9bdd96607043686985f0
Nicky Advokaat <nicky.advokaat@nubisonline.nl>
2015-12-15 16:34:04 +0100

    Updated networking

--- f4f/Classes/F4FNetworkController.swift
+++ f4f/Classes/F4FNetworkController.swift
@@ -10,9 +10,64 @@ import Foundation
 import FBSDKCoreKit
 import FBSDKLoginKit
 import Alamofire
+import AlamofireImage
 import SwiftyJSON
 
 class F4FNetworkController {
+    
+    private static var API_BASE = "https://foodspots.me/api"
+    
+    private static func getHeaders() -> [String: String] {
+        return [
+            "Authorization": "\(FBSDKAccessToken.currentAccessToken().tokenString)",
+            "Accept": "application/json"
+        ];
+    }
+    
+    private static func performRequest(method: Alamofire.Method, uri: String, parameters: [String: AnyObject]?, encoding: ParameterEncoding, callback: (JSON, Int) -> Void) {
+        Alamofire.request(method, API_BASE + uri, headers: getHeaders(), parameters: parameters, encoding: encoding).responseJSON { response in
+            switch(response.result) {
+            case .Success(let data):
+                callback(JSON(data), response.response!.statusCode)
+                break
+            case .Failure(let error):
+                if let resp = response.response{
+                    switch(resp.statusCode) {
+                    case 401:
+                        //Unauthorized
+                        break
+                    case 419:
+                        //Token expired
+                        break
+                    default:
+                        //Don't intercept, return the response
+                        let json = JSON(data: error.description.dataUsingEncoding(NSUTF8StringEncoding)!)
+                        callback(json, response.response!.statusCode)
+                        break
+                    }
+                }else{
+                    // No response, possibly no connection
+                }
+            }
+        }
+    }
+    
+    static func performRequest(method: Alamofire.Method, uri: String, parameters: [String: AnyObject]?, callback: (JSON, Int) -> Void) {
+        performRequest(method, uri: uri, parameters: parameters, encoding: .URL, callback: callback)
+    }
+    
+    static func performRequestJSONEncoding(method: Alamofire.Method, uri: String, parameters: [String: AnyObject]?, callback: (JSON, Int) -> Void) {
+        performRequest(method, uri: uri, parameters: parameters, encoding: .JSON, callback: callback)
+    }
+    
+    static func getImage(uri: String, callback: (UIImage) -> Void) {
+        Alamofire.request(.GET, uri).responseImage { response in
+            if let image = response.result.value {
+                callback(image)
+            }else{
+            }
+        }
+    }
     
     class func sendLocationUpdate(locationUpdate: LocationUpdate){
         
@@ -26,11 +81,11 @@ class F4FNetworkController {
         ]
         
         Alamofire.request(.POST, "https://foodspots.me/api/location/create", headers: headers, parameters: parameters)
-            .responseJSON { request, response, result in
-                switch (result) {
+            .responseJSON { response in
+                switch (response.result) {
                 case .Success(_):
                     break
-                case .Failure(_, let error):
+                case .Failure(let error):
                     print("Network error: \(error)")
                 }
         }
